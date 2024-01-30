34c8d5a8ba8b77e3ce4fb3716a6397f80bb42c86
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2024-01-30 21:51:36 +0530

    Create XPattern.java

---
+++ Star Patterns/XPattern.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Pattern {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int n=obj.nextInt();
+        pattern(n);
+    }
+    static void pattern(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                    if(i==j || i+j==n-1)
+                       System.out.print("* ");
+                    else
+                       System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+Output:
+5
+*       * 
+  *   *   
+    *     
+  *   *   
+*       * 
+
+
+*/
