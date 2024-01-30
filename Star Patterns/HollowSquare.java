1cca10ccfec37a764f13f38479ea3ef13d447592
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2024-01-30 22:45:23 +0530

    Create HollowSquare.java

---
+++ Star Patterns/HollowSquare.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Pattern {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int n=obj.nextInt();
+        pattern(n);
+    }
+    static void pattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                    if(j==1 || j==n || i==1 || i==n)
+                       System.out.print("* ");
+                    else
+                       System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+Output:
+7
+* * * * * * * 
+*           * 
+*           * 
+*           * 
+*           * 
+*           * 
+* * * * * * * 
+
+*/
