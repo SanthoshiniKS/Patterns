dc8bad924706589804ea60d905f2faf042be48ee
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2024-02-01 19:33:54 +0530

    Create InvertedTraingle.java

---
+++ Star Patterns/InvertedTraingle.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Pattern {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int n=obj.nextInt();
+        pattern(n);
+    }
+    static void pattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=(2*n)-1;j++){
+                if(j>=i && (i+j)<=2*n){
+                    if((i+j)%2==0)
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+                }
+            else
+                System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+Output:
+6
+*   *   *   *   *   * 
+  *   *   *   *   *   
+    *   *   *   *     
+      *   *   *       
+        *   *         
+          *           
+
+*/
