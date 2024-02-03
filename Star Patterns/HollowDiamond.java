8d58fed94279559fc836270866a162e360136693
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2024-02-03 18:15:13 +0530

    Create HollowDiamond.java

---
+++ Star Patterns/HollowDiamond.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Pattern {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n=obj.nextInt();
+        pattern(n);
+    }
+    public static void pattern(int n){
+        for(int i=1;i<=(2*n)-1;i++){
+            int k=i<=n?i:(2*n)-i;
+            for(int j=1;j<n+k;j++){
+                if(k+j==n+1 || j==n+k-1)
+                   System.out.print("*");
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+Output:
+Enter n: 
+5
+    *
+   * *
+  *   *
+ *     *
+*       *
+ *     *
+  *   *
+   * *
+    *
+
+*/
