048a6c6f766aaea5c097c61288d5b215519d294b
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2024-02-03 17:59:08 +0530

    Create TrianglePattern2.java

---
+++ Star Patterns/TrianglePattern2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Pattern {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n=obj.nextInt();
+        pattern(n);
+    }
+    public static void pattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<n+i;j++){
+                if(i+j<=n)
+                   System.out.print(" ");
+                else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+Output:
+Enter n: 
+5
+    *
+   ***
+  *****
+ *******
+*********
+
+*/
