7ab69b6f7042d11a4e9b5e08db6a036c6f12cf69
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2024-02-03 18:38:31 +0530

    Create RightPascalTriangle.java

---
+++ Star Patterns/RightPascalTriangle.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Pattern {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n=obj.nextInt();
+        pattern(n);
+    }
+    public static void pattern(int n){
+        for(int i=1;i<=(2*n)-1;i++){
+            int k=i<=n?i:(2*n)-i;
+            for(int j=1;j<=k;j++){
+                   System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+Output:
+Enter n: 
+5
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+*/
